2d2243c938522e292af32d4d5c8a7658b7e37569
bahriddin <bahriddin.abdiev@ferocia.com.au>
2021-07-22 15:15:52 +1000

    Build working version of the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+function App({ num }: { num: number }) {
+  const [isOver, setIsOver] = useState(false);
+  const [bits, setBits] = useState(Array(10).fill(0));
+
+  const handleClick = (position: number) => {
+    const newBits = bits.slice();
+    newBits[position] = newBits[position] === 1 ? 0 : 1;
+    setBits(newBits);
+
+    console.log(binaryToDecimal(newBits.join("")));
+
+    if (binaryToDecimal(newBits.join("")) === num) {
+      setIsOver(true);
+    }
+  };
+
+  if (!isOver) {
+    return (
+      <div className="app">
+        <DecimalInteger value={num} />
+        <Row bits={bits} handleClick={handleClick} />
+      </div>
+    );
+  } else {
+    return <GameOver />;
+  }
+}
+
+// binary string to decimal integer
+function binaryToDecimal(binary: string): number {
+  const decimal = binary.split("").reduce((acc, bit) => {
+    return acc * 2 + parseInt(bit, 2);
+  }, 0);
+  return decimal;
+}
+
+function DecimalInteger(props: { value: number }): React.ReactElement<{}> {
+  return <div className="decimal-integer">{props.value}</div>;
+}
+
+// Row's cell where we show the button to change the value
+// and position value in the bottom of the button
+function BinaryCell(props: {
+  position: number;
+  bit: number;
+  onClick: () => void;
+  key: number;
+}): React.ReactElement<{}> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cell" key={props.position}>
+      <button className="cell-button" onClick={props.onClick}>
+        {props.bit}
+      </button>
+      <div className="cell-position">{2 ** props.position}</div>
     </div>
   );
+}
+
+// show 10 BinaryCell in a row
+function Row(props: {
+  bits: number[];
+  handleClick: (position: number) => void;
+}) {
+  return (
+    <div className="row">
+      {props.bits.map((bit, index) => (
+        <BinaryCell
+          bit={bit}
+          position={9 - index}
+          key={index}
+          onClick={() => props.handleClick(index)}
+        />
+      ))}
+    </div>
+  );
+}
+
+function GameOver() {
+  return <div className="game-over">YOU WON!</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// Get random integer between 1 and 1023
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App num={getRandomInt(1, 31)} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
